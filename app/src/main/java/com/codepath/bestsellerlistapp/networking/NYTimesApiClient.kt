bb4fc714a1ca0b9d18bf2d51dd78524a264e00b8
Melissa Perez <melmperz@gmail.com>
2022-02-19 14:29:46 -0500

    update gradle properties and add NYT API key

--- app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.kt
+++ app/src/main/java/com/codepath/bestsellerlistapp/networking/NYTimesApiClient.kt
@@ -20,7 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory
  * after copy and paste the API key under your Account -> Apps -> <Your App> -> API Keys
 </Your> */
 // TODO: Replace the below API key with your own generated key
-private const val API_KEY = "<YOUR-API-KEY-GOES-HERE>"
+private const val API_KEY = "yvO1dNjuRXxN2nph6S0Uw6JLWToXaVBn"
 class NYTimesApiClient {
     private val nyTimesService: NYTimesService
 
